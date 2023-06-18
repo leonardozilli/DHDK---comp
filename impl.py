9e8dd3bec250f185c8304228f7a3f4d87ca0b534
leonardozilli <leonardo.zilli19@gmail.com>
2023-06-18 11:24:28 +0200

    added impl.py

---
+++ impl.py
@@ -0,0 +1,6 @@
+from data_model import IdentifiableEntity, EntityWithMetadata, Canvas, Collection, Image, Annotation, Manifest
+from RelDBProcessor import AnnotationProcessor, MetadataProcessor, RelationalQueryProcessor
+from GraphDBProcessor import CollectionProcessor, TriplestoreQueryProcessor
+from GenericQueryProcessor import GenericQueryProcessor
+
+
